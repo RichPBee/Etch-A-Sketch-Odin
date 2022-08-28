f7b6925e0ec18705fe97ccf82ab17d37bcf61937
ArrBee <ri.balfourth@gmail.com>
2022-08-29 00:01:22 +0100

    Full functionality complete.
    Added mouseover events for the grid squares so they change according to the current
    color selection. Can have colour via random hexcode or from rgb picker. Eraser button
    included to remove unwanted sections & a clear button to wipe the slate.
    Finally, there is also a slider to control the number of squares in the grid, which
    is stepped to different sizes from 1x1 to 100x100.

--- functionality.js
+++ functionality.js
@@ -1,10 +1,152 @@
+const gridContainer = document.querySelector(".grid-container");
+let currentColor = document.querySelector(".color-picker").value;
+
 const gridCreator = (value) => {
-    const gridSquare = document.createElement("div");
-    gridSquare.setAttribute("class", "grid-square")
-    const gridContainer = document.querySelector(".grid-container");
+    let gridWidth = Math.sqrt(value);
+    let squareWidth = 600/gridWidth;
+    let rgbBtn = document.querySelector("#rgb-randomiser");
     for (let i = 0; i < value; i ++) {
+        const gridSquare = document.createElement("div");
+        gridSquare.setAttribute("class", "grid-square");
+        gridSquare.style.height = `${squareWidth}px`;
+        gridSquare.style.width = `${squareWidth}px`;
+        gridSquare.id = `${i}`;
         gridContainer.append(gridSquare);
+        gridSquare.addEventListener("mouseover", () => {
+            if (rgbBtn.clicked == true) {
+                currentColor = generateRandomHex();
+                gridSquare.style.backgroundColor = currentColor;
+            } else {
+                gridSquare.style.backgroundColor = currentColor;
+            }
+        })
     }
 }
 
-gridCreator(16);
+const generateRandomHex = () => {
+    const hexVals = "0123456789ABCDEF";
+    let randomHex = "#";
+    for (let i = 0; i < 6; i++) {
+        randomHex += hexVals.charAt(Math.floor(Math.random() * hexVals.length));
+    }
+    console.log(randomHex);
+    return randomHex;
+}
+
+
+function genHexString(len) {
+    const hex = '0123456789ABCDEF';
+    let output = '';
+    for (let i = 0; i < len; ++i) {
+        output += hex.charAt(Math.floor(Math.random() * hex.length));
+    }
+    return output;
+}
+const clearGrid = () => {
+    const gridSquare = document.querySelectorAll(".grid-square");
+    gridSquare.forEach(square => {
+        gridContainer.removeChild(square);
+    })
+}
+
+const sliderValue = document.querySelector(".slider").value;
+
+const gridSize = (sliderValue) => {
+    let totalSquares;
+    if (sliderValue == 1) {
+        return totalSquares = 1;
+    } else if (sliderValue == 2) {
+        return totalSquares = 4;
+    } else if (sliderValue == 3) {
+        return totalSquares = 16;
+    } else if (sliderValue == 4) {
+        return totalSquares = 64;
+    } else if (sliderValue == 5) {
+        return totalSquares = 256;
+    } else if (sliderValue == 6) {
+        return totalSquares = 1024;
+    } else if (sliderValue == 7) {
+        return totalSquares = 4096;
+    } else if (sliderValue == 8) {
+        return totalSquares = 10000;
+    }
+}
+
+document.querySelector(".slider").addEventListener("change", () => {
+    const sliderValue = document.querySelector(".slider").value;
+    const sliderDisplay = document.querySelector(".slider-value");
+    clearGrid();
+    let gridSquares = gridSize(sliderValue);
+    console.log(gridSquares)
+    sliderDisplay.textContent = `${Math.sqrt(gridSquares)} x ${Math.sqrt(gridSquares)}`;
+    gridCreator(gridSquares);
+})
+
+document.querySelector(".color-picker").addEventListener("change", () => {
+    currentColor = document.querySelector(".color-picker").value;
+    resetButtons();
+    let monoBtn = document.querySelector("#monochrome");
+    currentColor = document.querySelector(".color-picker").value;
+    monoBtn.style.backgroundColor = "#8e8e8e";
+})
+
+document.querySelector("#monochrome").addEventListener("click", () => {
+    resetButtons();
+    let monoBtn = document.querySelector("#monochrome");
+    currentColor = document.querySelector(".color-picker").value;
+    monoBtn.style.backgroundColor = "#8e8e8e";
+})
+
+document.querySelector("#rgb-randomiser").addEventListener("click", () => {
+    resetButtons();
+    let rgbBtn = document.querySelector("#rgb-randomiser");
+    rgbBtn.clicked = true;
+    currentColor = "#000000";
+    rgbBtn.style.backgroundImage = "linear-gradient(to right, sienna, teal)"
+})
+
+document.querySelector("#eraser").addEventListener("click", () => {
+    resetButtons();
+    document.querySelector("#eraser").clicked = true;
+    let eraser = document.querySelector("#eraser");
+    currentColor = "white";
+    eraser.style.backgroundColor = "white";
+})
+
+document.querySelector("#clear").addEventListener("click", () => {
+    const clearBtn = document.querySelector("#clear");
+    clearBtn.style.backgroundColor = "white";
+    setTimeout(() => {
+        clearBtn.style.backgroundColor = "transparent";
+    }, "250");
+    const gridSquares = document.querySelectorAll(".grid-square");
+    gridSquares.forEach(square => {
+        square.style.backgroundColor = "white";
+    })
+    
+})
+
+const resetButtons = () => {
+    const buttonList = document.querySelectorAll("button");
+    buttonList.forEach(button => {
+        button.clicked = false;
+        button.style.backgroundColor = "transparent";
+        button.style.backgroundImage = "none";
+    })
+}
+
+
+window.addEventListener("load", () => {
+    gridCreator(1);
+    const sliderDisplay = document.querySelector(".slider-value");
+    sliderDisplay.textContent = `${1} x ${1}`;
+    document.querySelector("#monochrome").clicked = true;
+    let monoBtn = document.querySelector("#monochrome");
+    currentColor = document.querySelector(".color-picker").value;
+    monoBtn.style.backgroundColor = "#8e8e8e";
+})
+
+const painter = () => {
+
+}
+
